523f045fe8ffafa50672d0d87f621ae4fc6d6b1d
GalanCM <galan.c.m@gmail.com>
2017-09-21 17:18:10 -0400

    Get object nesting working.

--- src/Object.vue
+++ src/Object.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h2>{{ title }}</h2>
-    <div v-for="(value, key) in obj">
-      <!-- <div v-if=" value.hasOwnProperty('boolean') ">
+    <div v-for="(value, key) in obj"  :style=" {'padding-left': level*20+'px'} ">
+      <div v-if=" value.hasOwnProperty('boolean') ">
         b {{ key }}
       </div>
       <div v-else-if=" value.hasOwnProperty('number') ">
@@ -14,21 +14,25 @@
       <div v-else-if=" value.hasOwnProperty('enum') ">
         e {{ key }}
       </div>
-      <div v-else-if=" typeof(value) === "object" ">
-        {{value}}
-        <!-- <ObjectComponent :title="key" :obj="value"></ObjectComponent> -->
+      <div v-else-if=" value.hasOwnProperty('datetime') ">
+        d {{ key }}
       </div>
-      <!-- <div v-else>
-        <div>{{ key }}</div>
-      </div> -->
+      <div v-else-if=" Array.isArray(value) ">
+        array {{key}}
+      </div>
+      <div v-else-if=" typeof(value) === 'object' ">
+        <ObjectComponent :title="key" :obj="value" :level="level+1"></ObjectComponent>
+      </div>
+      <div v-else>
+        xx
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  
   export default {
     name: 'ObjectComponent',
-    props: ['title', 'obj']
+    props: ['title', 'obj', 'level']
   };
 </script>
